e7fa0e537a877583dd861f210cf0da265de236bf
Michael-elie <michael-elie@hotmail.com>
2024-01-19 17:10:01 +0100

    Premier pas

--- src/main.js
+++ src/main.js
@@ -1,20 +1,102 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var roleDeliverer = require('role.deliverer');
+
+
+
 
 module.exports.loop = function () {
     // You should spawn creeps at this point
-    
+
+
+    /**
+     * DELETE DEAD MINIONS MEMORY
+     */
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+    var deliverer = _.filter(Game.creeps, (creep) => creep.memory.role == 'deliverer');
+
+    /**
+     * HARVESTER SPAWNERS
+     */
+   /* if(harvesters.length < 3) {
+        var newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn_1'].spawnCreep([WORK,CARRY,MOVE], newName,
+            {memory: {role: 'harvester'}});
+    }*/
+
+    /**
+     *DELIVERER SPAWNERS
+     */
+     if(deliverer.length < 2) {
+         var newName = 'Deliverer' + Game.time;
+         console.log('Spawning new deliverer: ' + newName);
+         Game.spawns['Spawn_1'].spawnCreep([CARRY,MOVE,MOVE], newName,
+             {memory: {role: 'deliverer'}});
+     }
+
+
+    /**
+     * UPGRADER SPAWNERS
+     */
+    if(upgraders.length < 1) {
+        var newName = 'Upgrader' + Game.time;
+        console.log('Spawning new upgrader: ' + newName);
+        Game.spawns['Spawn_1'].spawnCreep([WORK,CARRY,MOVE], newName,
+            {memory: {role: 'upgrader'}});
+
+    }
+
+    /**
+     * BUILDERS SPAWNERS
+     */
+    if(builders.length < 2) {
+        var newName = 'Builder' + Game.time;
+        console.log('Spawning new builder: ' + newName);
+        Game.spawns['Spawn_1'].spawnCreep([WORK,CARRY,MOVE], newName,
+            {memory: {role: 'builder'}});
+    }
+
+
+    /**
+     * SPAWNERS VISUALS
+     */
+    if(Game.spawns['Spawn_1'].spawning) {
+        var spawningCreep = Game.creeps[Game.spawns['Spawn_1'].spawning.name];
+        Game.spawns['Spawn_1'].room.visual.text(
+            '🛠️' + spawningCreep.memory.role,
+            Game.spawns['Spawn_1'].pos.x + 1,
+            Game.spawns['Spawn_1'].pos.y,
+            {align: 'left', opacity: 0.8});
+    }
+
+    /**
+     * ATTRIBUTE MINIONS MISSIONS
+     */
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
+        if(creep.memory.role === 'harvester') {
             roleHarvester.run(creep);
         }
-        if(creep.memory.role == 'upgrader') {
+        if(creep.memory.role === 'upgrader') {
             roleUpgrader.run(creep);
         }
-        if(creep.memory.role == 'builder') {
+        if(creep.memory.role === 'builder') {
             roleBuilder.run(creep);
         }
     }
+
+
+
+
+
 }
--- src/role/upgrader.js
+++ src/role/upgrader.js
@@ -3,14 +3,16 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
+
         if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.upgrading = false;
-            creep.say('🔄 harvest');
+            creep.say('🔄 GO FARM !');
         }
         if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
             creep.memory.upgrading = true;
-            creep.say('⚡ upgrade');
+            creep.say('⚡ AMELIORONS !');
         }
+
 
         if(creep.memory.upgrading) {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
@@ -19,8 +21,8 @@ var roleUpgrader = {
         }
         else {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
     }
